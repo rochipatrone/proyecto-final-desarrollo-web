1265ec2b801a15406fe46f93977e8fcb7f8728d9
rochipatrone <patronerocio@gmail.com>
2022-08-20 23:54:16 -0300

    prueba php 1

--- envio.php
+++ envio.php
@@ -5,10 +5,10 @@ $apellido = $_POST ['name'];
 $email = $_POST ['mail']
 $mensaje = $_POST ['message'];
 
-$mensaje = "Este mensaje fue enviado por", $nombre , " \r\n";
-$mensaje = "Su e-mail es: " , $email , " \r\n";
-$mensaje = "Mensaje: " , $_POST['mensaje'] , " \r\n";
-$mensaje = "Enviado el " , date('d/m/y', time());
+$mensaje = "Este mensaje fue enviado por". $nombre . " \r\n";
+$mensaje = "Su e-mail es: " . $email . " \r\n";
+$mensaje = "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje = "Enviado el " . date('d/m/y', time());
 
 $destinatario = "patronerocio@gmail.com";
 $asunto = "Este mail fue enviado desde el Sitio Web Proyecto Final";
